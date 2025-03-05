5716ccae74d9e573717e86bb2c80ad5d908a6f71
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-03-05 10:30:18 +0000

    cubd

--- nvim/lua/luigi/core/global.lua
+++ nvim/lua/luigi/core/global.lua
@@ -6,8 +6,8 @@ vim.g.RTP = vim.split(vim.o.runtimepath, ",")[1]
 vim.g.RC = os.getenv("HOME") .. "/.config/nvim"
 
 -- 42 Header Settings
-vim.g.user42 = 'luigi'
-vim.g.mail42 = 'luigi@student.42porto.com'
+vim.g.user42 = 'lude-bri'
+vim.g.mail42 = 'lude-bri@student.42porto.com'
 
 -- Set PATH
 vim.cmd("set path=.,**")
--- nvim/lua/luigi/plugins/42header.lua
+++ nvim/lua/luigi/plugins/42header.lua
@@ -1,6 +1,12 @@
 return {
 	"42Paris/42header",
 	lazy = true,
+  opts = {
+    default_map = true, -- Default mapping <F1> in normal mode.
+    auto_update = true, -- Update header when saving.
+    user = "lude-bri", -- Your user.
+    mail = "lude-brir@student.42porto.com", -- Your mail.
+  },
 	keys = {
 		{ "<F1>", ":StdHeader<CR>", desc = "Insert 42 header" },
 	}
